1e6093eb0d6b351badd8b617924fbc4048311af3
Chris Hafey <chafey@gmail.com>
2014-04-18 11:27:57 -0500

    added RGB decoding

---
+++ dist/colorImageDecoder.js
@@ -0,0 +1,34 @@
+/*! colorImageDecoder - v0.1.0 - 2014-04-18 | (c) 2014 Chris Hafey | https://github.com/chafey/colorImageDecoder*/
+var colorImageDecoder = (function (colorImageDecoder) {
+
+    "use strict";
+
+    if(cornerstone === undefined) {
+        cornerstone = {};
+    }
+
+    function decodeRGB(rgbBuffer, rgbaBuffer) {
+        if(rgbBuffer === undefined) {
+            throw "decodeRGB: rgbBuffer must not be undefined";
+        }
+        if(rgbBuffer.length % 3 !== 0) {
+            throw "decodeRGB: rgbBuffer length must be divisble by 3";
+        }
+
+        var numPixels = rgbBuffer.length / 3;
+        var rgbIndex = 0;
+        var rgbaIndex = 0;
+        for(var i= 0; i < numPixels; i++) {
+            rgbaBuffer[rgbaIndex++] = rgbBuffer[rgbIndex++]; // red
+            rgbaBuffer[rgbaIndex++] = rgbBuffer[rgbIndex++]; // green
+            rgbaBuffer[rgbaIndex++] = rgbBuffer[rgbIndex++]; // blue
+            rgbaBuffer[rgbaIndex++] = 255; //alpha
+        }
+
+    }
+
+    // module exports
+    colorImageDecoder.decodeRGB = decodeRGB;
+
+    return colorImageDecoder;
+}(colorImageDecoder));
